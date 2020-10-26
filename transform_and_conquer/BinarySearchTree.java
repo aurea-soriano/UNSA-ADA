33e665fe627aa97542dff41971ade77468109e9d
Aurea Soriano-Vargas <aurea.soriano@gmail.com>
2020-10-26 20:10:53 -0300

    print

--- transform_and_conquer/BinarySearchTree.java
+++ transform_and_conquer/BinarySearchTree.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 class BinarySearchTree{
 
     class Node{
@@ -47,6 +51,85 @@ class BinarySearchTree{
         printInOrderRecursive(node.right);
       }
     }
+    int maxLevel(Node node) {
+        if (node == null)
+            return 0;
+
+        return Math.max(maxLevel(node.left), maxLevel(node.right)) + 1;
+    }
+    void printNode() {
+       int maxLevel = maxLevel(root);
+
+       printNodeInternal(Collections.singletonList(root), 1, maxLevel);
+   }
+  boolean isAllElementsNull(List<Node> list) {
+        for (Node node : list) {
+            if (node != null)
+                return false;
+        }
+
+        return true;
+    }
+    void printWhitespaces(int count) {
+        for (int i = 0; i < count; i++)
+            System.out.print(" ");
+    }
+   void printNodeInternal(List<Node> nodes, int level, int maxLevel) {
+        if (nodes.isEmpty() || isAllElementsNull(nodes))
+            return;
+
+        int floor = maxLevel - level;
+        int endgeLines = (int) Math.pow(2, (Math.max(floor - 1, 0)));
+        int firstSpaces = (int) Math.pow(2, (floor)) - 1;
+        int betweenSpaces = (int) Math.pow(2, (floor + 1)) - 1;
+
+        printWhitespaces(firstSpaces);
+
+        List<Node> newNodes = new ArrayList<Node>();
+        for (Node node : nodes) {
+            if (node != null) {
+                System.out.print(node.key);
+                newNodes.add(node.left);
+                newNodes.add(node.right);
+            } else {
+                newNodes.add(null);
+                newNodes.add(null);
+                System.out.print(" ");
+            }
+
+            printWhitespaces(betweenSpaces);
+        }
+        System.out.println("");
+
+        for (int i = 1; i <= endgeLines; i++) {
+            for (int j = 0; j < nodes.size(); j++) {
+                printWhitespaces(firstSpaces - i);
+                if (nodes.get(j) == null) {
+                    printWhitespaces(endgeLines + endgeLines + i + 1);
+                    continue;
+                }
+
+                if (nodes.get(j).left != null)
+                    System.out.print("/");
+                else
+                    printWhitespaces(1);
+
+                printWhitespaces(i + i - 1);
+
+                if (nodes.get(j).right != null)
+                    System.out.print("\\");
+                else
+                    printWhitespaces(1);
+
+                printWhitespaces(endgeLines + endgeLines - i);
+            }
+
+            System.out.println("");
+        }
+
+        printNodeInternal(newNodes, level + 1, maxLevel);
+    }
+
 
 
     public static void main(String[] args){
@@ -71,7 +154,7 @@ class BinarySearchTree{
 
       tree.printInOrder();
 
-
+      tree.printNode();
       System.out.println("*************");
 
 
@@ -102,7 +185,7 @@ class BinarySearchTree{
       tree.add(160);
 
       tree.printInOrder();
-
+      tree.printNode();
     }
 
 }
--- transform_and_conquer/RedBlackTree.java
+++ transform_and_conquer/RedBlackTree.java
@@ -1,5 +1,7 @@
 //TreeMap -> Red Black TreeMap
-
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 class RedBlackTree {
   private static final boolean RED = true;
   private static final boolean BLACK = false;
@@ -145,6 +147,88 @@ class RedBlackTree {
     }
   }
 
+  int maxLevel(Node node) {
+      if (node == null)
+          return 0;
+
+      return Math.max(maxLevel(node.left), maxLevel(node.right)) + 1;
+  }
+  void printNode() {
+     int maxLevel = maxLevel(root);
+
+     printNodeInternal(Collections.singletonList(root), 1, maxLevel);
+ }
+boolean isAllElementsNull(List<Node> list) {
+      for (Node node : list) {
+          if (node != null)
+              return false;
+      }
+
+      return true;
+  }
+  void printWhitespaces(int count) {
+      for (int i = 0; i < count; i++)
+          System.out.print(" ");
+  }
+ void printNodeInternal(List<Node> nodes, int level, int maxLevel) {
+      if (nodes.isEmpty() || isAllElementsNull(nodes))
+          return;
+
+      int floor = maxLevel - level;
+      int endgeLines = (int) Math.pow(2, (Math.max(floor - 1, 0)));
+      int firstSpaces = (int) Math.pow(2, (floor)) - 1;
+      int betweenSpaces = (int) Math.pow(2, (floor + 1)) - 1;
+
+      printWhitespaces(firstSpaces);
+
+      List<Node> newNodes = new ArrayList<Node>();
+      for (Node node : nodes) {
+          if (node != null) {
+              System.out.print(node.key);
+              newNodes.add(node.left);
+              newNodes.add(node.right);
+          } else {
+              newNodes.add(null);
+              newNodes.add(null);
+              System.out.print(" ");
+          }
+
+          printWhitespaces(betweenSpaces);
+      }
+      System.out.println("");
+
+      for (int i = 1; i <= endgeLines; i++) {
+          for (int j = 0; j < nodes.size(); j++) {
+              printWhitespaces(firstSpaces - i);
+              if (nodes.get(j) == null) {
+                  printWhitespaces(endgeLines + endgeLines + i + 1);
+                  continue;
+              }
+
+              if (nodes.get(j).left != null)
+                  System.out.print("/");
+              else
+                  printWhitespaces(1);
+
+              printWhitespaces(i + i - 1);
+
+              if (nodes.get(j).right != null)
+                  System.out.print("\\");
+              else
+                  printWhitespaces(1);
+
+              printWhitespaces(endgeLines + endgeLines - i);
+          }
+
+          System.out.println("");
+      }
+
+      printNodeInternal(newNodes, level + 1, maxLevel);
+  }
+
+
+
+
 
   public static void main(String[] args){
     RedBlackTree tree = new RedBlackTree();
@@ -167,7 +251,7 @@ class RedBlackTree {
     tree.add(160);
 
     tree.printInOrder();
-
+    tree.printNode();
 
     System.out.println("*************");
 
@@ -199,7 +283,7 @@ class RedBlackTree {
     tree.add(160);
 
     tree.printInOrder();
-
+    tree.printNode();
   }
 
 
