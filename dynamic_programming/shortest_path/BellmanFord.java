192c30a6238e8674dff2164399abdedc1432b5a0
Aurea Soriano-Vargas <aurea.soriano@gmail.com>
2020-11-05 11:05:43 -0300

    shortest path

--- dynamic_programming/shortest_path/BellmanFord.java
+++ dynamic_programming/shortest_path/BellmanFord.java
@@ -1,159 +1,153 @@
-// vamos a implementar bellmanford usando un objeto arista
-// objeto arista-> origen el fin y el peso
+// vamos a implementar bellmanford, crear un objeto arista
+// objeto arista-> origen, fin y peso
 // acepta pesos en negativo
 
 import java.io.*;
 import java.util.*;
 
 class BellmanFord{
-  final static int INF = 99999;
-  int vNumber; // numero de vertices
-  int eNumber; // numero de aristas
-  Edge edge[];
+    final static int INF = 99999;
+    int vNumber; // numero de vertices
+    int eNumber; //numero de aristas
+    Edge edge[];
 
-  class Edge{
-    int source, destination, weight;
-    Edge(){
-      source = destination = weight = INF;
+    class Edge{
+      int source, destination, weight;
+      Edge(){
+        source = destination = weight = INF;
+      }
+    };
+
+    BellmanFord(int v, int e){
+      vNumber  = v;
+      eNumber = e;
+      edge = new Edge[e];
+      for(int i=0; i<e; i++){
+        edge[i] = new Edge();
+      }
     }
-  };
 
-  BellmanFord(int v, int e){
-    vNumber = v;
-    eNumber = e;
-    edge = new Edge[e];
-    for (int i=0; i<e; i++){
-      edge[i] = new Edge();
-    }
-  }
+    //funcion utilitaria para imprimir nuestros resultados
+    void printResult(int dist[], int d){
+      System.out.println("Nodo \t\t Distancia al origen");
 
-//una funcion utilitaria para imprimir nuestros resultados
-  void printSolution(int dist[], int d){
-    System.out.println("Nodo \t\t Distancia al origen");
-
-    for(int i=0; i<vNumber; i++){
-      if(d==-1){
-        System.out.println(i+"\t\t"+dist[i]);
-      }else{
-        if(d==i){
-            System.out.println(i+"\t\t"+dist[i]);
-            return;
+      for(int i=0; i<vNumber; i++){
+        if(d==-1 || d==i){
+           System.out.println(i+"\t\t"+dist[i]);
         }
       }
     }
-  }
+
+    // recorrido de BellmanFord
+    void traversal(int s, int d){
+      // si d es igual a -1, vamos a calcular la menor distancia
+      // desde el punto de origen hacia todos los otros nodos
 
 
-  // recorrido de BellmanFord
-  void traversal(int s, int d){
-    // s es el nodo que es punto de partida y d es el nodo de destino
-    // si d es -1 retornar'a todos los destinos a partir de ese nodo s
+      // vector que va a contener nuestras distancias mas cortas de ese s para ese default:
+      int dist[] = new int[vNumber];
 
-    // un vector de salida que va a contener todas las distancias
-    // mas cortas de ese s para todo ese d
-    int dist[] = new int[vNumber];
+      //1) inicializar todas las distancias como infinito
+      for(int i =0 ; i<vNumber; i++){
+        dist[i] = INF;
+      }
 
-    // 1) inicializar todas las distancias como infinito
-    for(int i=0; i< vNumber; i++){
-      dist[i] = INF;
-    }
+      //la distancia del nodo al mismo nodo
+      dist[s] = 0;
 
-    //la distancia del nodo al mismo nodo
-    dist[s] = 0;
+      //2) recorrer para encontrar una ruta mas corta desde el punto
+      // de partida hacia cualquier otro nodo.
 
-    //2) recorrer para encontrar una ruta mas corta desde el punto
-    // de partida hacia cualquier otro nodo
+      for(int i=0; i<vNumber-1; i++){
+        for(int j=0; j<eNumber; j++){
+          int u = edge[j].source;
+          int v = edge[j].destination;
+          int weight = edge[j].weight;
 
-    for(int i=1; i< vNumber; i++){
-      for(int j=0; j< eNumber; j++){
+          //si existe una distancia o sea si hay una conexion
+          // si nuestro nuevo peso es menor al peso de destination
+          if( dist[u]!=INF && dist[u]+weight < dist[v]){
+              dist[v] = dist[u] + weight;
+          }
+        }
+      }
+
+      //3) identificar si hay ciclos negativos infinitos
+      for(int j=0; j<eNumber; j++){
         int u = edge[j].source;
         int v = edge[j].destination;
-        int weight  = edge[j].weight;
+        int weight = edge[j].weight;
 
-        if(dist[u] != INF && dist[u]+weight < dist[v]){
-          dist[v] = dist[u] + weight;
+        if( dist[u]!=INF && dist[u]+weight < dist[v]){
+            System.out.println("Se ha detectado un ciclo negativo");
+            return;
         }
+
       }
+
+      //imprimimos el resultado
+      printResult(dist,d);
+
     }
 
-    //3) identificar si hay pesos en negativo
-    for(int j=0; j< eNumber; j++){
-      int u = edge[j].source;
-      int v = edge[j].destination;
-      int weight  = edge[j].weight;
-      // estamos en el caso que una distancia hacia si misma va a ser menor que 0
-      if(dist[u] != INF && dist[u]+weight < dist[v]){
-         System.out.println("Se ha detectado un ciclo infinito");
-         return;
+      public static void main(String args[]){
+
+        /**
+                      0
+                 (4)/    \ (8)
+                   1      7
+              (12) |      | (-9)
+          (14) 8 - 2      6
+              (19) |      | (11)
+                   3      5
+                          \ (-21)
+                          4
+                  **/
+        int vNumber = 9;
+        int eNumber = 8;
+
+
+        BellmanFord bellmanFord = new BellmanFord(vNumber, eNumber);
+
+        bellmanFord.edge[0].source = 0;
+        bellmanFord.edge[0].destination = 1;
+        bellmanFord.edge[0].weight = 4;
+
+        bellmanFord.edge[1].source = 0;
+        bellmanFord.edge[1].destination = 7;
+        bellmanFord.edge[1].weight = 8;
+
+        bellmanFord.edge[2].source = 1;
+        bellmanFord.edge[2].destination = 2;
+        bellmanFord.edge[2].weight = 12;
+
+        bellmanFord.edge[3].source = 2;
+        bellmanFord.edge[3].destination = 3;
+        bellmanFord.edge[3].weight = 19;
+
+        bellmanFord.edge[4].source = 2;
+        bellmanFord.edge[4].destination = 8;
+        bellmanFord.edge[4].weight = 14;
+
+        bellmanFord.edge[5].source = 7;
+        bellmanFord.edge[5].destination = 6;
+        bellmanFord.edge[5].weight = -9;
+
+        bellmanFord.edge[6].source = 6;
+        bellmanFord.edge[6].destination = 5;
+        bellmanFord.edge[6].weight = 11;
+
+        bellmanFord.edge[7].source = 5;
+        bellmanFord.edge[7].destination = 4;
+        bellmanFord.edge[7].weight = -21;
+
+        int source = 0;
+        bellmanFord.traversal(source, -1);
+        int destination = 4;
+        System.out.println("Desde el nodo origen "+source+" hasta el nodo destino "+destination);
+        bellmanFord.traversal(source, destination);
+
+
       }
-    }
-
-      //imprimir el resultado
-      printSolution(dist, d);
-  }
-
-  public static void main(String args[]){
-
-    /**
-               0
-          (4)/    \ (8)
-            1      7
-       (12) |      | (-9)
-   (14) 8 - 2      6
-       (19) |      | (11)
-            3      5
-                   \ (-21)
-                   4
-           **/
-
-      int vNumber = 9;
-      int eNumber = 8;
-
-
-      BellmanFord bellmanFord = new BellmanFord(vNumber, eNumber);
-
-      bellmanFord.edge[0].source = 0;
-      bellmanFord.edge[0].destination = 1;
-      bellmanFord.edge[0].weight = 4;
-
-      bellmanFord.edge[1].source = 0;
-      bellmanFord.edge[1].destination = 7;
-      bellmanFord.edge[1].weight = 8;
-
-      bellmanFord.edge[2].source = 1;
-      bellmanFord.edge[2].destination = 2;
-      bellmanFord.edge[2].weight = 12;
-
-      bellmanFord.edge[3].source = 2;
-      bellmanFord.edge[3].destination = 3;
-      bellmanFord.edge[3].weight = 19;
-
-      bellmanFord.edge[4].source = 2;
-      bellmanFord.edge[4].destination = 8;
-      bellmanFord.edge[4].weight = 14;
-
-      bellmanFord.edge[5].source = 7;
-      bellmanFord.edge[5].destination = 6;
-      bellmanFord.edge[5].weight = -9;
-
-      bellmanFord.edge[6].source = 6;
-      bellmanFord.edge[6].destination = 5;
-      bellmanFord.edge[6].weight = 11;
-
-      bellmanFord.edge[7].source = 5;
-      bellmanFord.edge[7].destination = 4;
-      bellmanFord.edge[7].weight = -21;
-
-
-
-      int source = 0;
-      bellmanFord.traversal(source, -1);
-      int destination = 4;
-      System.out.println("Desde el nodo origen "+source+" hasta el nodo destino "+destination);
-      bellmanFord.traversal(source, destination);
-
-
-  }
-
 
 }
--- dynamic_programming/shortest_path/Dijkstra.java
+++ dynamic_programming/shortest_path/Dijkstra.java
@@ -1,126 +1,118 @@
-// vamos a implementar dijkstra en una representacion de
-//matriz
+//vamos a implementar dijkstra en una representacion de matriz
 // solo recibe pesos positivos
+
 import java.io.*;
 import java.util.*;
 
+
 class Dijkstra{
-  int vNumber;
+  int vNumber; // nu'mero de ve'rtices
   int graphMatrix[][];
   final static int INF = 99999;
 
-
   Dijkstra(int n, int[][] graph){
-    vNumber = n;
+    vNumber  = n;
     graphMatrix = graph;
   }
 
-  //funcion utilitaria para retornar el nodo con la minima distancia
-  int minDistance(int dist[], boolean includedPath[]){
-    //inicializar nuestro valores minimos
-    int min = INF, min_index = -1;
 
-    for (int i=0; i<vNumber; i++){
-      if(includedPath[i]==false && dist[i]<=min){
-        min = dist[i];
-        min_index = i;
-      }
-    }
-    return min_index;
-  }
+   //funcion utilitaria para retornar el nodo con la minima distancia
+   int minDistance(int dist[], boolean includedPath[]){
 
-//una funcion utilitaria para imprimir nuestros resultados
-  void printSolution(int dist[], int d){
-    System.out.println("Nodo \t\t Distancia al origen");
+     //inicializar nuestro valor minimo
+     int min = INF, min_node = -1;
 
-    for(int i=0; i<vNumber; i++){
-      if(d==-1){
-        System.out.println(i+"\t\t"+dist[i]);
-      }else{
-        if(d==i){
-            System.out.println(i+"\t\t"+dist[i]);
-            return;
-        }
-      }
-    }
-  }
+     for(int i =0; i<vNumber; i++){
+       if(includedPath[i]==false && dist[i]<=min){
+         min = dist[i];
+         min_node = i;
+       }
+     }
+     return min_node;
+   }
 
 
-  // recorrido de Dijkstra
-  void traversal(int s, int d){
-    // s es el nodo que es punto de partida y d es el nodo de destino
-    // si d es -1 retornar'a todos los destinos a partir de ese nodo s
+   //funcion utilitaria para imprimir nuestros resultados
+   void printResult(int dist[], int d){
+     System.out.println("Nodo \t\t Distancia al origen");
 
-    // un vector de salida que va a contener todas las distancias
-    // mas cortas de ese s para todo ese d
+     for(int i=0; i<vNumber; i++){
+       if(d==-1 || d==i){
+          System.out.println(i+"\t\t"+dist[i]);
+       }
+     }
+   }
+
+
+  //recorrido de dijkstra
+  void traversal(int s, int d){ //si d es igual a -1 entonces vamos a retornar
+    // las distancias hacia todos los otros nodos, caso contrario
+    // retornamos solo la distancia hasta el nodo d
+
+    // vector que va a contener nuestras distancias mas cortas de ese s para ese default:
     int dist[] = new int[vNumber];
 
-
-    //vamos a tener un array donde vamos a colocar si un nodo es
-    //parte de nuestro camino mas corto
+    // array donde colocaremos si un nodo es parte de nuestro
+    //camino mas corto
     boolean includedPath[] = new boolean[vNumber];
 
-
-    //inicializar todas las distancias con infinito
+    // inicializar todas las distancias con infinito
     // y todos nuestros includedPath como falso
+
     for(int i=0; i<vNumber; i++){
       dist[i] = INF;
       includedPath[i] = false;
     }
 
-      //la distancia del nodo hacia ese mismo nodo
-      dist[s] = 0;
-
-      //vamos a encontrar todas las minimas distancias
-      for(int count=0; count< vNumber-1; count++){
-
-          //Escoger el nodo con la distancia minima
-          // del conjunto de nodos que no fueron procesados
-          int u = minDistance(dist, includedPath);//minima distancia
-
-          //marcar ese nodo u como procesado
-          includedPath[u] = true;
+    // la distancia del nodo hacia el mismo nodo
+    dist[s] = 0;
 
 
-          //actualizar los velores de las distancias de los nodos
-          //adyacentes del nodo escogido
+    //vamos a encontrar todas las distancias minimos
+    for(int count=0; count < vNumber-1; count++){
 
-          for(int v=0; v<vNumber; v++){
+        // escoger el nodo con la distancia minima del conjunto
+        // de nodos que no fueron procesados, que estan con
+        // falso dentro del includedPath
+        int u = minDistance(dist, includedPath); // nodo de la minima distancia
 
-            // actualizar dist[v] si y solo si no esta' en includedPath
-            // hay una arista entre u y v, y el peso total de la ruta
-            //entre s y  v a traves de u es m'as pequenho que el valor
-            //actual de dist[v]
+        // marcamos ese nodo u como procesado
+        includedPath[u] = true;
 
-            if(!includedPath[v] && graphMatrix[u][v]!=INF && dist[u]!=INF && dist[u]+graphMatrix[u][v]<dist[v] ){
-              dist[v] = dist[u] + graphMatrix[u][v];
-            }
+        //actualizamos los vectores de las distancias
+        // de los nodos adyacentes del nodo escogido
 
+        for(int v=0; v<vNumber; v++){
+          // actualizar dist[v] si y solo si no esta en includedPath
+          // hay una arista entre u y v, y el peso de la ruta entre
+          // s y v a traves de u es mas pequenho que el valor
+          //actual de dist[v]
 
-
+          if(!includedPath[v] && graphMatrix[u][v]!=INF && dist[u]+graphMatrix[u][v]<dist[v]){
+            dist[v] = dist[u] + graphMatrix[u][v];
           }
-      }
+        }
+    }
 
-      //imprimir el resultado
-      printSolution(dist, d);
+    //imprimir el resultado
+    printResult(dist, d);
+
   }
 
   public static void main(String args[]){
 
     /**
-               0
-          (4)/    \ (8)
-            1      7
-       (12) |      | (9)
-   (14) 8 - 2      6
-       (19) |      | (11)
-            3      5
-                   \ (21)
-                   4
-           **/
-
-
-      int graph[][] = new int[][] { { INF, 4, INF, INF, INF, INF, INF, 8, INF },
+                  0
+             (4)/    \ (8)
+               1      7
+          (12) |      | (9)
+      (14) 8 - 2      6
+          (19) |      | (11)
+               3      5
+                      \ (21)
+                      4
+              **/
+    int graph[][] = new int[][] { { INF, 4, INF, INF, INF, INF, INF, 8, INF },
                           { INF, INF, 12, INF, INF, INF, INF, INF, INF },
                           { INF, INF, INF, 19, INF, INF, INF, INF, 14 },
                           { INF, INF, INF, INF, INF, INF, INF, INF, INF },
@@ -130,15 +122,14 @@ class Dijkstra{
                           { INF, INF, INF, INF, INF, INF, 9, INF, INF },
                           { INF, INF, INF, INF, INF, INF, INF, INF, INF } };
 
-      Dijkstra dijkstra = new Dijkstra(9, graph);
-      int source = 0;
-      dijkstra.traversal(source, -1);
-      int destination = 4;
-      System.out.println("Desde el nodo origen "+source+" hasta el nodo destino "+destination);
-      dijkstra.traversal(source, destination);
+    Dijkstra dijkstra = new Dijkstra(9, graph);
+    int source = 0;
+    dijkstra.traversal(source, -1);
+    int destination = 4;
+    System.out.println("Desde el nodo origen "+source+" hasta el nodo destino "+destination);
+    dijkstra.traversal(source, destination);
 
 
   }
-
 
 }
